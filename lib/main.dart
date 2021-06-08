fb4a2a5b286a6726f2a27edb4857c3359f5121ea
KellyEgesa <kelly.egesa@gmail.com>
2021-06-08 20:19:34 +0300

    Added import feature  - Question  - Widget building

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,54 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questions = [
-    "Whats your favourite color?",
-    "Whats your favourite animal?",
-  ];
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "Whats your favourite color?",
+      "Whats your favourite animal?",
+      "Whats your favourite team?",
+    ];
+
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text("My First App"),
       ),
       body: Column(children: [
-        Text("The Question!"),
-        RaisedButton(
+        Question(
+          questions[questionIndex],
+        ),
+        ElevatedButton(
           child: Text("Answer 1"),
-          onPressed: null,
+          onPressed: answerQuestion,
         ),
-        RaisedButton(
+        ElevatedButton(
           child: Text("Answer 2"),
-          onPressed: null,
+          onPressed: answerQuestion,
         ),
-        RaisedButton(
+        ElevatedButton(
           child: Text("Answer 3"),
-          onPressed: null,
+          onPressed: answerQuestion,
         )
       ]),
     ));
