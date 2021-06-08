63f26bfa63e4f7cf8da4fa661f4fddf3f1fbb8bc
KellyEgesa <kelly.egesa@gmail.com>
2021-06-08 12:09:24 +0300

    Added button lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questions = [
+    "Whats your favourite color?",
+    "Whats your favourite animal?",
+  ];
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("My First Application"),);
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
+      body: Column(children: [
+        Text("The Question!"),
+        RaisedButton(
+          child: Text("Answer 1"),
+          onPressed: null,
+        ),
+        RaisedButton(
+          child: Text("Answer 2"),
+          onPressed: null,
+        ),
+        RaisedButton(
+          child: Text("Answer 3"),
+          onPressed: null,
+        )
+      ]),
+    ));
   }
 }
