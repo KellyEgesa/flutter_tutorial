37291e53c1d30dea6233744a975eea97a96ddd76
KellyEgesa <kelly.egesa@gmail.com>
2021-06-08 10:10:07 +0300

    Added Main class

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text("My First Application"),);
+  }
+}
