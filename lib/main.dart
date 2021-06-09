dac4706fa3b6a4e0ae00991484b303d0ace76bf0
KellyEgesa <kelly.egesa@gmail.com>
2021-06-09 23:08:51 +0300

    Added conditions

--- lib/main.dart
+++ lib/main.dart
@@ -15,44 +15,51 @@ class _MyApp extends StatefulWidget {
 class _MyAppState extends State<_MyApp> {
   var _questionIndex = 0;
 
+  final questions = [
+    {
+      "questionText": "What's your favourite color?",
+      "answers": ["Black", "Red", "Green", "White"],
+    },
+    {
+      "questionText": "What's your favourite animal?",
+      "answers": ["Dog", "Cat", "Tortoise", "Snake"]
+    },
+    {
+      "questionText": "What's your favourite team?",
+      "answers": ["Arsenal", "Liverpool", "Chelsea", "Leicester"]
+    }
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < questions.length) {
+      print("We have more questions");
+    }
 
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": "What's your favourite color?",
-        "answers": ["Black", "Red", "Green", "White"],
-      },
-      {
-        "questionText": "What's your favourite animal?",
-        "answers": ["Dog", "Cat", "Tortoise", "Snake"]
-      },
-      {
-        "questionText": "What's your favourite team?",
-        "answers": ["Arsenal", "Liverpool", "Chelsea", "Leicester"]
-      }
-    ];
-
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text("My First App"),
       ),
-      body: Column(children: [
-        Question(
-          questions[_questionIndex]['questionText'],
-        ),
-        ...(questions[_questionIndex]["answers"] as List<String>)
-            .map((answer) => Answer(_answerQuestion, answer))
-            .toList()
-      ]),
+      body: _questionIndex < questions.length
+          ? Column(children: [
+              Question(
+                questions[_questionIndex]['questionText'],
+              ),
+              ...(questions[_questionIndex]["answers"] as List<String>)
+                  .map((answer) => Answer(_answerQuestion, answer))
+                  .toList()
+            ])
+          : Center(
+              child: Text("No more Questions"),
+            ),
     ));
   }
 }
